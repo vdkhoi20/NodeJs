3a95ab9bc72b1e0718770c8754efc665094d54dd
vdkhoi20 <vdkhoi.itai@gmail.com>
2022-11-12 11:46:35 +0700

    try argument of event module

--- event_module.js
+++ event_module.js
@@ -1,11 +1,11 @@
 const event_module=require("events");
 var class_event= new event_module();
 
-class_event.on("make noise",function(){
-    console.log("catch event make noise")
+class_event.on("make noise",function(arg){
+    console.log("catch event make noise "+arg.make_noise_nth);
 });
 
 
 class_event.emit("silent"); // nothing happen
-class_event.emit("make noise");
-class_event.emit("make noise");
+class_event.emit("make noise",{make_noise_nth:"first time"});
+class_event.emit("make noise",{make_noise_nth: "second time"});
